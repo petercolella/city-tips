996b1d23a2052735cbe5bb54cb1d2899a451cec6
Ray Meibaum <rmeibaum@me.com>
2017-04-27 15:38:04 -0400

    post show working

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -5,6 +5,7 @@ class CitiesController < ApplicationController
 
   def show
     @city = City.find(params[:id])
+    @posts = @city.posts
   end
   #
   # def create
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,8 +4,8 @@ class PostsController < ApplicationController
 
   def show
     @post = Post.find(params[:id])
-    @author = User.find(@post.user_id)
-    @city = City.find(@post.city_id)
+    @author = @post.user
+    @city = @post.city
   end
 
   def new
@@ -14,9 +14,9 @@ class PostsController < ApplicationController
   end
 
   def create
-    binding.pry
     post = Post.create(post_params)
-    redirect_to city_post_path(params[:city_id])
+    city = post.city
+    redirect_to city_post_path(city, post)
   end
 
   private
