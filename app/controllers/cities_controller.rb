bd1b25ff0925b81042993a0226ce26cf5e45e437
Ray Meibaum <rmeibaum@me.com>
2017-04-27 11:24:05 -0400

    city views working

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -1,9 +1,11 @@
 class CitiesController < ApplicationController
   def index
+    @cities = City.all
   end
 
-  # def show
-  # end
+  def show
+    @city = City.find(params[:id])
+  end
   #
   # def create
   # end
