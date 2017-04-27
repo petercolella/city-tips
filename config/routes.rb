8214f11cf50847b7be2437febed557f3ed66d9ec
Ray Meibaum <rmeibaum@me.com>
2017-04-27 10:08:27 -0400

    add models

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
+  get 'users/show'
+
+  get 'posts/index'
+
+  get 'posts/show'
+
+  devise_for :users
   get '/' => 'cities#index'
 
+  root to: "cities#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
