34374cd7af4c960c00b43c0728c4eada2179f3a0
Ray Meibaum <rmeibaum@me.com>
2017-05-01 09:50:24 -0400

    comments and new cities index

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
 Rails.application.routes.draw do
+  # Devise route setup
   devise_for :users, controllers: {
 	registrations: 'users/registrations'
 }
-
+  # Setting the root route
   root to: "static_pages#welcome"
 
+  # Generate default routes for Users
   resources :users
+
+  # Generate nested routes for Cities and Posts
   resources :cities do
     resources :posts
   end
